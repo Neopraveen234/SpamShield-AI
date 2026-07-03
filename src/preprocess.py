a5ef699b6fc29f251ecde9daf01935f55cebe6f2
Praveenkanth M <praveenkanth248@gmail.com>
2026-07-03 13:19:32 +0530

    Fix NLTK deployment issue

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,8 +1,13 @@
 import re 
 import string
+import nltk
 
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+
+
+nltk.download("stopwords")
+nltk.download("wordnet")
 
 stop_words=set(stopwords.words("english"))
 lemmatizer=WordNetLemmatizer()
